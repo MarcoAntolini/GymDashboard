f52e11b8bae8e4e7b76b98367f385f1bbf26300c
Marco Antolini <marco.antolini2001@gmail.com>
2023-08-17 18:41:49 +0200

    add function to create database

--- app/lib/src/main/java/dashboard/database/ConnectionProvider.java
+++ app/lib/src/main/java/dashboard/database/ConnectionProvider.java
@@ -3,12 +3,14 @@ package dashboard.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Expose a utility method to connect to a MySQL database.
  */
 public final class ConnectionProvider {
 
+	private static final String HOST_URI = "jdbc:mysql://localhost:3306/";
 	private final String username;
 	private final String password;
 	private final String databaseName;
@@ -22,6 +24,13 @@ public final class ConnectionProvider {
 		this.username = username;
 		this.password = password;
 		this.databaseName = databaseName;
+		try (Connection connection = DriverManager.getConnection(HOST_URI, this.username, this.password);
+				Statement statement = connection.createStatement()) {
+			String createDatabaseQuery = String.format("CREATE DATABASE IF NOT EXISTS `%s`", this.databaseName);
+			statement.executeUpdate(createDatabaseQuery);
+		} catch (SQLException e) {
+			throw new IllegalStateException("Could not establish a connection with " + HOST_URI, e);
+		}
 	}
 
 	/**
@@ -32,7 +41,7 @@ public final class ConnectionProvider {
 		final String dbUri = "jdbc:mysql://localhost:3306/" + this.databaseName;
 		try {
 			return DriverManager.getConnection(dbUri, this.username, this.password);
-		} catch (final SQLException e) {
+		} catch (SQLException e) {
 			throw new IllegalStateException("Could not establish a connection with " + dbUri, e);
 		}
 	}
