1bc4fb34c31d876539ab755b8a1d9f848d16dcad
Marco Antolini <marco.antolini2001@gmail.com>
2023-08-28 04:17:48 +0200

    Update TableClienteTest.java

--- app/lib/src/test/java/dashboard/TableClienteTest.java
+++ app/lib/src/test/java/dashboard/TableClienteTest.java
@@ -1,8 +1,12 @@
 package dashboard;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
@@ -24,9 +28,9 @@ class TableClienteTest {
 	static final TableCliente table = new TableCliente(connectionProvider.getMySQLConnection());
 
 	final Cliente cliente1 = new Cliente("123456789012345", "John", "Doe", Utils.buildSqlDate(1990, 1, 1),
-			Utils.buildSqlDate(2021, 1, 1));
+			Utils.buildSqlDate(2021, 1, 1)).addEmail("johndoe@gmail.com");
 	final Cliente cliente2 = new Cliente("987654321098765", "Jane", "Smith", Utils.buildSqlDate(1995, 5, 10),
-			Optional.of("1234567890"), Optional.of("janesmith@gmail.com"), Utils.buildSqlDate(2021, 1, 1));
+			"1234567890", "janesmith@gmail.com", Utils.buildSqlDate(2021, 1, 1));
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -50,32 +54,47 @@ class TableClienteTest {
 	@Test
 	void saveTest() {
 		assertTrue(table.save(cliente1));
-		// assertFalse(table.save(cliente1));
 		assertTrue(table.save(cliente2));
 	}
 
-	// @Test
-	// void findByPrimaryKeyTest() {
-	// Cliente cliente = new Cliente("1234567890123456", "John", "Doe",
-	// Utils.buildSqlDate(1990, 1, 1));
-	// assertTrue(table.save(cliente));
+	@Test
+	void updateTest() {
+		table.save(cliente2);
+		final Cliente updatedCliente2 = new Cliente("987654321098765", "Jonny", "Smith",
+				Utils.buildSqlDate(1995, 5, 10), "1234567890", "janesmith@gmail.com",
+				Utils.buildSqlDate(2023, 1, 1));
+		assertTrue(table.update(2,
+				Map.of("codiceFiscale", "987654321098765", "nome", "Jonny", "cognome", "Smith", "dataNascita",
+						Utils.buildSqlDate(1995, 5, 10), "telefono", "1234567890", "email", "janesmith@gmail.com",
+						"dataIscrizione", Utils.buildSqlDate(2023, 1, 1))));
+		final Optional<Cliente> foundCliente = table.findByPrimaryKey(updatedCliente2.getId());
+		assertFalse(foundCliente.isEmpty());
+		assertEquals(updatedCliente2.getDataNascita(), foundCliente.get().getDataNascita());
+	}
 
-	// assertEquals(cliente,
-	// table.findByPrimaryKey(cliente.getCodiceFiscale()).orElse(null));
-	// }
+	@Test
+	void deleteTest() {
+		table.save(cliente1);
+		assertTrue(table.delete(cliente1.getId()));
+		assertFalse(table.delete(cliente1.getId()));
+		assertTrue(table.findByPrimaryKey(cliente1.getId()).isEmpty());
+	}
 
-	// @Test
-	// void findAllTest() {
-	// Cliente cliente1 = new Cliente("1234567890123456", "John", "Doe",
-	// Utils.buildSqlDate(1990, 1, 1));
-	// Cliente cliente2 = new Cliente("9876543210987654", "Jane", "Smith",
-	// Utils.buildSqlDate(1995, 5, 10));
-	// assertTrue(table.save(cliente1));
-	// assertTrue(table.save(cliente2));
+	@Test
+	void findByPrimaryKeyTest() {
+		table.save(cliente1);
+		table.save(cliente2);
+		assertEquals(cliente1, table.findByPrimaryKey(cliente1.getId()).orElse(null));
+		assertEquals(cliente2, table.findByPrimaryKey(cliente2.getId()).orElse(null));
+	}
 
-	// assertIterableEquals(
-	// List.of(cliente1, cliente2),
-	// table.findAll());
-	// }
+	@Test
+	void findAllTest() {
+		table.save(cliente1);
+		table.save(cliente2);
+		assertIterableEquals(
+				List.of(cliente1, cliente2),
+				table.findAll());
+	}
 
 }
