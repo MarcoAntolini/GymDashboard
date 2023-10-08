ee546735e9e0a8a15bdfe54e20d46420fa0c824a
Marco Antolini <marco.antolini2001@gmail.com>
2023-10-08 18:01:31 +0200

    update all the column names

--- app/lib/src/main/java/dashboard/ui/panels/tablePanels/ColumnNames.java
+++ app/lib/src/main/java/dashboard/ui/panels/tablePanels/ColumnNames.java
@@ -5,6 +5,31 @@ public final class ColumnNames {
 	private ColumnNames() {
 	}
 
+	protected static final String[] COLUMNS_ABBONAMENTI = new String[] {
+			"Codice",
+			"Durata"
+	};
+
+	protected static final String[] COLUMNS_ACQUISTI = new String[] {
+			"Id cliente",
+			"Data e ora",
+			"Importo",
+			"Tipo",
+			"Codice"
+	};
+
+	protected static final String[] COLUMNS_ATTREZZATURE = new String[] {
+			"Id",
+			"Descrizione",
+			"Fornitore"
+	};
+
+	protected static final String[] COLUMNS_BOLLETTE = new String[] {
+			"Id",
+			"Descrizione",
+			"Fornitore"
+	};
+
 	protected static final String[] COLUMNS_CLIENTI = new String[] {
 			"Id",
 			"Codice fiscale",
@@ -17,13 +42,74 @@ public final class ColumnNames {
 			"Provincia",
 			"Telefono",
 			"Email",
-			"Data di iscrizione"
+			"Data di iscrizione",
+			"Entrate rimaste"
+	};
+
+	protected static final String[] COLUMNS_CONTRATTI = new String[] {
+			"Id dipendente",
+			"Tipo",
+			"Costo orario",
+			"Data di inizio",
+			"Data di fine"
 	};
 
 	protected static final String[] COLUMNS_DIPENDENTI = new String[] {
-
+			"Id",
+			"Codice fiscale",
+			"Nome",
+			"Cognome",
+			"Data di nascita",
+			"Via",
+			"N°",
+			"Città",
+			"Provincia",
+			"Telefono",
+			"Email",
+			"Data di assunzione"
 	};
 
-	// TODO l'ordine qui deve essere uguale a toArray e al database
+	protected static final String[] COLUMNS_INGRESSI = new String[] {
+			"Id cliente",
+			"Data e ora"
+	};
+
+	protected static final String[] COLUMNS_INTERVENTI = new String[] {
+			"Id",
+			"Descrizione",
+			"Fornitore",
+			"Data di inizio",
+			"Data di fine"
+	};
+
+	protected static final String[] COLUMNS_LISTINI = new String[] {
+			"Anno",
+			"Tipo",
+			"Codice",
+			"Prezzo"
+	};
+
+	protected static final String[] COLUMNS_PACCHETTI_ENTRATE = new String[] {
+			"Codice",
+			"Numero di entrate",
+	};
+
+	protected static final String[] COLUMNS_PAGAMENTI = new String[] {
+			"Id",
+			"Data e ora",
+			"Importo",
+			"Tipo"
+	};
+
+	protected static final String[] COLUMNS_STIPENDI = new String[] {
+			"Id",
+			"Id dipendente"
+	};
+
+	protected static final String[] COLUMNS_TIMBRATURE = new String[] {
+			"Id dipendente",
+			"Entrata",
+			"Uscita"
+	};
 
 }
