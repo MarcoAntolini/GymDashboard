f5d9a9924faddf73fe384aca218b6e494d96dac3
Marco Antolini <marco.antolini2001@gmail.com>
2023-10-20 10:42:48 +0200

    add utility method

--- app/lib/src/main/java/dashboard/utils/Screen.java
+++ app/lib/src/main/java/dashboard/utils/Screen.java
@@ -1,6 +1,12 @@
 package dashboard.utils;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Taskbar;
+import java.awt.Taskbar.Feature;
 import java.awt.Toolkit;
 
 public final class Screen {
@@ -17,6 +23,20 @@ public final class Screen {
 	public static final Dimension MAXIMUM_SIZE = new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	private Screen() {
+	}
+
+	public static void setIcon(final JFrame frame, final String fileName) {
+		final ImageIcon appIcon = new ImageIcon(fileName);
+		frame.setIconImage(appIcon.getImage());
+		if (Taskbar.isTaskbarSupported()) {
+			final Taskbar taskbar = Taskbar.getTaskbar();
+			if (taskbar.isSupported(Feature.ICON_IMAGE)) {
+				final Toolkit toolkit = Toolkit.getDefaultToolkit();
+				final Class<?> thisClass = Screen.class;
+				final Image dockIcon = toolkit.getImage(thisClass.getClassLoader().getResource(fileName));
+				taskbar.setIconImage(dockIcon);
+			}
+		}
 	}
 
 }
